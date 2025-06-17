4f9a86ef8785990b27ba42d37b722e64b637332c
shashi <shashisandysh@gmail.com>
2025-06-17 18:26:26 +0530

    2 ttestcases commited

---
+++ HRMORANGE_GIT/src/test/java/com/OrangeHRM/runnerScripts/Demo123.java
@@ -0,0 +1,13 @@
+package com.OrangeHRM.runnerScripts;
+
+import org.testng.annotations.Test;
+
+public class Demo123 
+{
+	
+	@Test
+	public void add1() {
+		System.out.println("hiii");
+	}
+
+}
---
+++ HRMORANGE_GIT/src/test/java/com/OrangeHRM/runnerScripts/Demo345.java
@@ -0,0 +1,13 @@
+package com.OrangeHRM.runnerScripts;
+
+import org.testng.annotations.Test;
+
+public class Demo345 {
+
+	
+	@Test
+	public void demo()
+	{
+		System.out.println("hiiii");
+	}
+}
